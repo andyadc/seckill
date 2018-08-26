dc34b717cdc378be97aa4cb5b26dc1fcb6781f1b
andyadc <andaicheng@gmail.com>
2018-08-26 21:55:03 +0800

    Initial project

---
+++ src/main/java/com/andyadc/seckill/Startup.java
@@ -0,0 +1,15 @@
+package com.andyadc.seckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author andaicheng
+ * @since 2018/8/26
+ */
+@SpringBootApplication
+public class Startup {
+    public static void main(String[] args) {
+        SpringApplication.run(Startup.class);
+    }
+}
---
+++ src/test/java/com/andyadc/seckill/test/Test.java
@@ -0,0 +1,8 @@
+package com.andyadc.seckill.test;
+
+/**
+ * @author andaicheng
+ * @since 2018/8/26
+ */
+public class Test {
+}
