9a8857ba628510d70742fac727bf0e303520210b
99BILL\andy.an <andy.an@99bill.com>
2018-08-27 18:24:38 +0800

    seckill

---
+++ src/main/java/com/andyadc/seckill/result/Result.java
@@ -0,0 +1,45 @@
+package com.andyadc.seckill.result;
+
+/**
+ * @author andy.an
+ * @since 2018/8/27
+ */
+public class Result<T> {
+
+    private int code;
+    private String message;
+    private T data;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Result{" +
+                "code=" + code +
+                ", message=" + message +
+                ", data=" + data +
+                "}";
+    }
+}
