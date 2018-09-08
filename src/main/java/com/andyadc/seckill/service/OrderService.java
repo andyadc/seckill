255af323871c2c81d9703b287052dabf732aa94b
99BILL\andy.an <andy.an@99bill.com>
2018-09-08 09:52:38 +0800

    seckill

---
+++ src/main/java/com/andyadc/seckill/service/OrderService.java
@@ -0,0 +1,16 @@
+package com.andyadc.seckill.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author andy.an
+ * @since 2018/9/6
+ */
+@Service
+public class OrderService {
+
+    private static final Logger logger = LoggerFactory.getLogger(OrderService.class);
+
+}
