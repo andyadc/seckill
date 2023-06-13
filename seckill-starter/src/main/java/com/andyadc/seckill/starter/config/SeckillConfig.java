b767781b952a73c978c3ca7ae65d52c2b06b79a0
andyadc <andaicheng@gmail.com>
2023-06-13 10:53:07 +0800

    seckill

--- seckill-starter/src/main/java/com/andyadc/seckill/starter/config/SeckillConfig.java
+++ seckill-starter/src/main/java/com/andyadc/seckill/starter/config/SeckillConfig.java
@@ -1,9 +1,11 @@
 package com.andyadc.seckill.starter.config;
 
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+@ServletComponentScan(value = {"com.andyadc.seckill"})
 @ComponentScan(value = {"com.andyadc.seckill"})
 @Configuration
 @Import({RedisConfig.class})
